24bef8111a66fed2f5894cc29ab1c4e035edfdb0
joellaedwards <jmedw857@byu.edu>
2025-03-07 15:58:18 -0700

    fix rook

--- shared/src/main/java/chess/piecemoves/RookMoves.java
+++ shared/src/main/java/chess/piecemoves/RookMoves.java
@@ -12,21 +12,27 @@ public class RookMoves {
 
     public ArrayList<ChessMove> returnRookMoves(ChessBoard board, ChessPosition startingPosition, ChessGame.TeamColor myColor) {
         ArrayList<ChessMove> availableMoves = new ArrayList<>();
-        int startingRow = startingPosition.getRow();
-        int startingCol = startingPosition.getColumn();
-        int rowNum = startingRow;
-        int colNum = startingCol;
 
-        // straight until you hit a piece
-        // going up!
+        getMovesInDirections(board, startingPosition, 1, 0, myColor, availableMoves);
+        getMovesInDirections(board, startingPosition, -1, 0, myColor, availableMoves);
+        getMovesInDirections(board, startingPosition, 0, 1, myColor, availableMoves);
+        getMovesInDirections(board, startingPosition, 0, -1, myColor, availableMoves);
 
-        while (rowNum < 8) {
-            rowNum++;
+        return availableMoves;
+    }
+
+    public void getMovesInDirections(ChessBoard board, ChessPosition startingPosition, int rowInc, int colInc, ChessGame.TeamColor myColor, ArrayList<ChessMove> availableMoves) {
+        int rowNum = startingPosition.getRow();
+        int colNum = startingPosition.getColumn();
+
+        while(rowNum + rowInc <= 8 && rowNum + rowInc >= 1 && colNum + colInc <= 8 && colNum + colInc >=1) {
+            rowNum = rowNum + rowInc;
+            colNum = colNum + colInc;
             ChessPosition currPosition = new ChessPosition(rowNum, colNum);
             ChessPiece pieceAtCurrPosition = board.getPiece(currPosition);
             if (pieceAtCurrPosition != null) {
                 if (pieceAtCurrPosition.getTeamColor() == myColor) {
-                    if (rowNum - 1 >= 1 && (rowNum - 1) != startingRow) {
+                    if ((rowNum - rowInc) != startingPosition.getRow() && (colNum - colInc) != startingPosition.getColumn()) {
                         ChessPosition backUpPosition = new ChessPosition(rowNum - 1, colNum);
                         ChessMove currMove = new ChessMove(startingPosition, backUpPosition, null);
                         availableMoves.add(currMove);
@@ -37,101 +43,20 @@ public class RookMoves {
                 }
                 else {
                     ChessMove currMove = new ChessMove(startingPosition, currPosition, null);
-                    availableMoves.add(currMove);
+                    if (!availableMoves.contains(currMove)) {
+                        availableMoves.add(currMove);
+                    }
                 }
                 break;
             }
             ChessMove currMove = new ChessMove(startingPosition, currPosition, null);
-            availableMoves.add(currMove);
-        }
-
-        rowNum = startingRow;
-        while (rowNum > 1) {
-            rowNum--;
-            ChessPosition currPosition = new ChessPosition(rowNum, colNum);
-            ChessPiece pieceAtCurrPosition = board.getPiece(currPosition);
-            if (pieceAtCurrPosition != null) {
-                if (pieceAtCurrPosition.getTeamColor() == myColor) {
-                    if (rowNum + 1 <= 8 && (rowNum + 1) != startingRow) {
-                        ChessPosition backUpPosition = new ChessPosition(rowNum + 1, colNum);
-                        ChessMove currMove = new ChessMove(startingPosition, backUpPosition, null);
-                        if (!availableMoves.contains(currMove)) {
-                            availableMoves.add(currMove);
-                        }
-                    }
-                    else {
-                        break;
-                    }
-                }
-                else {
-                    ChessMove currMove = new ChessMove(startingPosition, currPosition, null);
-                    availableMoves.add(currMove);
-                }
-                break;
-            }
-            else {
-                ChessMove currMove = new ChessMove(startingPosition, currPosition, null);
+            if (!availableMoves.contains(currMove)) {
                 availableMoves.add(currMove);
             }
         }
 
-        rowNum = startingRow;
-        while (colNum < 8) {
-            colNum++;
-            ChessPosition currPosition = new ChessPosition(rowNum, colNum);
-            ChessPiece pieceAtCurrPosition = board.getPiece(currPosition);
-            if (pieceAtCurrPosition != null) {
-                if (pieceAtCurrPosition.getTeamColor() == myColor) {
-                    if (colNum - 1 >= 1 && (colNum - 1) != startingCol) {
-                        ChessPosition backUpPosition = new ChessPosition(rowNum, colNum - 1);
-                        ChessMove currMove = new ChessMove(startingPosition, backUpPosition, null);
-                        if (!availableMoves.contains(currMove)) {
-                            availableMoves.add(currMove);
-                        }
-                    }
-                    else {
-                        break;
-                    }
-                }
-                else {
-                    ChessMove currMove = new ChessMove(startingPosition, currPosition, null);
-                    availableMoves.add(currMove);
-                }
-                break;
-            }
-            ChessMove currMove = new ChessMove(startingPosition, currPosition, null);
-            availableMoves.add(currMove);
-        }
 
-        colNum = startingCol;
-        while (colNum > 1) {
-            colNum--;
-            ChessPosition currPosition = new ChessPosition(rowNum, colNum);
-            ChessPiece pieceAtCurrPosition = board.getPiece(currPosition);
-            if (pieceAtCurrPosition != null) {
-                if (pieceAtCurrPosition.getTeamColor() == myColor) {
-                    if (colNum + 1 <= 8 && (colNum + 1) != startingCol) {
-                        ChessPosition backUpPosition = new ChessPosition(rowNum, colNum + 1);
-                        ChessMove currMove = new ChessMove(startingPosition, backUpPosition, null);
-                        if (!availableMoves.contains(currMove)) {
-                            availableMoves.add(currMove);
-                        }
-                    }
-                    else {
-                        break;
-                    }
-                }
-                else {
-                    ChessMove currMove = new ChessMove(startingPosition, currPosition, null);
-                    availableMoves.add(currMove);
-                }
-                break;
-            }
-            ChessMove currMove = new ChessMove(startingPosition, currPosition, null);
-            availableMoves.add(currMove);
-        }
 
-        return availableMoves;
     }
 
 }
