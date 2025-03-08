fe5f99bebbb60359fb3746709e005bd98a472bb3
joellaedwards <jmedw857@byu.edu>
2025-03-07 17:57:15 -0700

    pawn functions

--- shared/src/main/java/chess/piecemoves/PawnMoves.java
+++ shared/src/main/java/chess/piecemoves/PawnMoves.java
@@ -2,6 +2,7 @@ package chess.piecemoves;
 
 import chess.*;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class PawnMoves {
@@ -10,64 +11,36 @@ public class PawnMoves {
         ArrayList<ChessMove> availableMoves = new ArrayList<>();
         int startingRow = startingPosition.getRow();
         int startingColumn = startingPosition.getColumn();
+        ChessGame.TeamColor otherColor;
 
 
         // if its the first time the pawn is moved it can be moved forward 2 squares
 
         if (myColor == ChessGame.TeamColor.WHITE) {
+            otherColor = ChessGame.TeamColor.BLACK;
             if (startingRow == 2) {
                 ChessPosition oneUp = new ChessPosition(3, startingColumn);
                 ChessPosition currPosition = new ChessPosition(4, startingColumn);
-
                 canMoveTwo(board, currPosition, oneUp, startingPosition, myColor, availableMoves);
             }
 
 
             // take diagonally
             ChessPosition diagonalPosition = new ChessPosition(startingRow + 1, startingColumn + 1);
-            if (isAcceptableMove(diagonalPosition, startingPosition)) {
-                ChessPiece pieceAtDiagonal = board.getPiece(diagonalPosition);
-                if (pieceAtDiagonal != null) {
-                    if (pieceAtDiagonal.getTeamColor() == ChessGame.TeamColor.BLACK) {
-                        if (canBePromoted(diagonalPosition, myColor)) {
-                            promotePiece(startingPosition, diagonalPosition, availableMoves);
-                        } else {
-                            ChessMove currMove = new ChessMove(startingPosition, diagonalPosition, null);
-                            availableMoves.add(currMove);
-                        }
-                    }
-                }
-            }
+            evalDiagonalMove(board, startingPosition, myColor, availableMoves, diagonalPosition, otherColor);
+
             ChessPosition otherDiagonal = new ChessPosition(startingRow + 1, startingColumn - 1);
-            if (isAcceptableMove(otherDiagonal, startingPosition)) {
-                ChessPiece otherPiece = board.getPiece(otherDiagonal);
-                if (otherPiece != null) {
-                    if (otherPiece.getTeamColor() == ChessGame.TeamColor.BLACK) {
-                        if (canBePromoted(otherDiagonal, myColor)) {
-                            promotePiece(startingPosition, otherDiagonal, availableMoves);
-                        } else {
-                            ChessMove currMove = new ChessMove(startingPosition, otherDiagonal, null);
-                            availableMoves.add(currMove);
-                        }
-                    }
-                }
-            }
+            evalDiagonalMove(board, startingPosition, myColor, availableMoves, otherDiagonal, otherColor);
 
             // just go up one
             ChessPosition currPosition = new ChessPosition(startingRow + 1, startingColumn);
-            if (isAcceptableMove(currPosition, startingPosition) && !isOccupied(board, currPosition)) {
-                if (canBePromoted(currPosition, myColor)) {
-                    promotePiece(startingPosition, currPosition, availableMoves);
-                } else {
-                    ChessMove currMove = new ChessMove(startingPosition, currPosition, null);
-                    availableMoves.add(currMove);
-                }
-            }
+            evalMove(board, startingPosition, availableMoves, currPosition, myColor);
 
         }
 
 
         if (myColor == ChessGame.TeamColor.BLACK) {
+            otherColor = ChessGame.TeamColor.WHITE;
             if (startingRow == 7) {
                 ChessPosition downJustOne = new ChessPosition(6, startingColumn);
                 ChessPosition currPosition = new ChessPosition(5, startingColumn);
@@ -77,54 +50,50 @@ public class PawnMoves {
 
             // take diagonally
             ChessPosition diagonalPosition = new ChessPosition(startingRow - 1, startingColumn + 1);
-            if (isAcceptableMove(diagonalPosition, startingPosition)) {
-                ChessPiece pieceAtDiagonal = board.getPiece(diagonalPosition);
-                if (pieceAtDiagonal != null) {
-                    if (pieceAtDiagonal.getTeamColor() == ChessGame.TeamColor.WHITE) {
-                        if (canBePromoted(diagonalPosition, myColor)) {
-                            promotePiece(startingPosition, diagonalPosition, availableMoves);
-                        } else {
-                            ChessMove currMove = new ChessMove(startingPosition, diagonalPosition, null);
-                            availableMoves.add(currMove);
-                        }
-                    }
-                }
-            }
+            evalDiagonalMove(board, startingPosition, myColor, availableMoves, diagonalPosition, otherColor);
 
             ChessPosition otherDiagonal = new ChessPosition(startingRow - 1, startingColumn - 1);
-            if (isAcceptableMove(otherDiagonal, startingPosition)) {
-                ChessPiece otherPiece = board.getPiece(otherDiagonal);
-                if (otherPiece != null) {
-                    if (otherPiece.getTeamColor() == ChessGame.TeamColor.WHITE) {
-                        if (canBePromoted(otherDiagonal, myColor)) {
-                            promotePiece(startingPosition, otherDiagonal, availableMoves);
-                        } else {
-                            ChessMove currMove = new ChessMove(startingPosition, otherDiagonal, null);
-                            availableMoves.add(currMove);
-                        }
-                    }
-                }
-            }
+            evalDiagonalMove(board, startingPosition, myColor, availableMoves, otherDiagonal, otherColor);
 
             // just go down one
             ChessPosition currPosition = new ChessPosition(startingRow - 1, startingColumn);
-            if (isAcceptableMove(currPosition, startingPosition) && !isOccupied(board, currPosition)) {
-                if (canBePromoted(currPosition, myColor)) {
-                    promotePiece(startingPosition, currPosition, availableMoves);
-                } else {
-                    ChessMove currMove = new ChessMove(startingPosition, currPosition, null);
-                    availableMoves.add(currMove);
-                }
-            }
-
-
-            // take diagonally
+            evalMove(board, startingPosition, availableMoves, currPosition, myColor);
 
         }
             return availableMoves;
         }
 
+    private void evalDiagonalMove(ChessBoard board, ChessPosition startingPosition, ChessGame.TeamColor myColor,
+                          ArrayList<ChessMove> availableMoves, ChessPosition diagonalPosition, ChessGame.TeamColor otherColor) {
+        if (isAcceptableMove(diagonalPosition, startingPosition)) {
+            ChessPiece pieceAtDiagonal = board.getPiece(diagonalPosition);
+            if (pieceAtDiagonal != null) {
+                if (pieceAtDiagonal.getTeamColor() == otherColor) {
+                    if (canBePromoted(diagonalPosition, myColor)) {
+                        promotePiece(startingPosition, diagonalPosition, availableMoves);
+                    } else {
+                        ChessMove currMove = new ChessMove(startingPosition, diagonalPosition, null);
+                        availableMoves.add(currMove);
+                    }
+                }
+            }
+        }
+    }
 
+    private void evalMove(ChessBoard board, ChessPosition startingPosition,
+                          ArrayList<ChessMove> availableMoves, ChessPosition position, ChessGame.TeamColor myColor) {
+        if (isAcceptableMove(position, startingPosition)) {
+            ChessPiece pieceHere = board.getPiece(position);
+            if (pieceHere == null) {
+                    if (canBePromoted(position, myColor)) {
+                        promotePiece(startingPosition, position, availableMoves);
+                    } else {
+                        ChessMove currMove = new ChessMove(startingPosition, position, null);
+                        availableMoves.add(currMove);
+                    }
+            }
+        }
+    }
 
     public boolean isAcceptableMove(ChessPosition position, ChessPosition startingPosition) {
         if (position.getRow() < 1 || position.getRow() > 8 || position.getColumn() < 1 || position.getColumn() > 8) {
